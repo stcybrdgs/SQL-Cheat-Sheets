5520ebe160b15a60cc4b371f6fc02f456accc31f
Stacy Bridges <stcybrdgs@gmail.com>
2018-10-14 13:42:22 -0500

    git commit

--- mySQLrefresher_sets1-10.sql
+++ mySQLrefresher_sets1-10.sql
@@ -1,5 +1,5 @@
 -- MySQL REFRESHER 
-
+-- sets 1-10
 
 -- ///////////////////////////////////////////
 -- SELECT w/arithmetic operators and concept that NULL is not a value 
@@ -24,39 +24,42 @@ UPDATE customer SET address = '123 Guitar Street' WHERE id =5;
 INSERT INTO customer (name, address, city, state, zip) VALUES ('Stacy Bridges', '604 Country Aire Dr', 'Round Rock', 'TX', '78664');
 SELECT * FROM customer;
 
-
 -- ///////////////////////////////////////////
--- use DELETE to remove rows from existing table
-DELETE FROM customer WHERE id > 7;
-DELETE FROM customer WHERE id = 3;
--- use UPDATE and SET to modify data in existing row
-UPDATE customer SET name ='0',address = '0' WHERE id = 7;
--- use nested SELECT to copy info from neighboring row into target row
-UPDATE customer SET name = 'Jazz Winston', address = (SELECT address FROM customer WHERE id = 6) WHERE id = 7;
-SELECT * FROM customer;
-
-
--- ///////////////////////////////////////////
--- use CREATE TABLE to create a new table
+-- USING CREATE, INSERT, DELETE
 -- rem syntax requires: ( colName dataType, colName dataType, ... )
 CREATE TABLE books(
  Title TEXT,
  Author TEXT,
  Year INTEGER 
 );
+
 -- populate new table with values
 INSERT INTO books VALUES('Stacy', 'Book One', 2018);
 INSERT INTO books VALUES('Tobey', 'SciFi Thriller', 2018);
 SELECT * FROM books;
+
 -- use DELETE FROM to remove rows from the target table
 DELETE FROM books WHERE Author = 'Stacy'
+
 -- use DROP TABLE to delete a table from the local database
 DROP TABLE IF EXISTS books;
 SELECT * FROM books;
 
+-- use DELETE to remove rows from existing table
+DELETE FROM customer WHERE id > 7;
+DELETE FROM customer WHERE id = 3;
+
+-- use UPDATE and SET to modify data in existing row
+UPDATE customer SET name ='0',address = '0' WHERE id = 7;
+
+-- use nested SELECT to copy info from neighboring row into target row
+UPDATE customer SET name = 'Jazz Winston', address = (SELECT address FROM customer WHERE id = 6) WHERE id = 7;
+SELECT * FROM customer;
 
 -- ///////////////////////////////////////////
+-- USING CREATE AND ALTER
 -- Remember that NULL is not a value, it is the lack of a value
+-- CREATE and INSERT
 CREATE TABLE test3(
  a INTEGER NOT NULL, -- NULL constraint
  b TEXT NOT NULL,    -- NULL constraint
@@ -65,21 +68,20 @@ CREATE TABLE test3(
 INSERT INTO test3 VALUES ( 1, 'this', 'that');
 SELECT * FROM test3;
 
-
--- ///////////////////////////////////////////
+-- CREATE and INSERT
 SELECT name FROM sqlite_master WHERE type='table';
 CREATE TABLE test1 ( a TEXT, b TEXT, c TEXT );
 INSERT INTO test1 VALUES ( 'one', 'four', 'seven' );
 INSERT INTO test1 VALUES ( 'two', 'five', 'eight' );
 INSERT INTO test1 VALUES ( 'three', 'six', 'nine' );
+
 -- use ALTER TABLE to add columns to an existing database:
 ALTER TABLE test1 ADD d TEXT;
 ALTER TABLE test1 ADD e TEXT;
 SELECT * FROM test1;
 
-
 -- ///////////////////////////////////////////
--- working with Boolean expressions...
+-- USING BOOLEAN EXPRESSIONS
 -- use AND, OR to combine Boolean expressions together
 SELECT Name, Continent, Population
 FROM Country
